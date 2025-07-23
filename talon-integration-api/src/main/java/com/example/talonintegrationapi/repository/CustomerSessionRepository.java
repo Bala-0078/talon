ef694b5782e9850cb46729414614fbf002ad3cb7
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-23 15:01:43 +0530

    Delete talon-integration-api directory

--- talon-integration-api/src/main/java/com/example/talonintegrationapi/repository/CustomerSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.talonintegrationapi.repository;
-
-import com.example.talonintegrationapi.model.CustomerSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CustomerSessionRepository extends JpaRepository<CustomerSession, Long> {
-    Optional<CustomerSession> findBySessionId(String sessionId);
-}
